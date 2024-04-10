ea8cfe7f4465eee5e2b21433c8ca33b63c9d1de1
ZackZack2121 <164313435+ZackZack2121@users.noreply.github.com>
2024-04-10 10:35:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const diceElement = document.getElementById("dice");
+const rollButton = document.getElementById("rollDice");
+const rollHistory = document.getElementById("rollHistory");
+
+let rollHistoryList = [];
+
+function rollDice () {
+    result = Math.floor(Math.random()*6) + 1;
+    face = getDiceFace(result);
+
+    rollHistoryList.push(face);
+    updateRollHistoryList();
+    return face;
+}
+
+function updateRollHistoryList () {
+    rollHistory.innerHTML = "";
+
+    for (let i = 0; i < rollHistoryList.length; i++) {
+        listElement = document.createElement("li");
+        listElement.innerHTML = `Roll ${i+1}: <span class="rollHistoryDice"> ${rollHistoryList[i]} </span>`;
+        rollHistory.appendChild(listElement);
+    }
+}
+
+function getDiceFace (result) {
+    switch (result) {
+        case 1:
+            return "⚀";
+        case 2:
+            return "⚁";
+        case 3:
+            return "⚂";
+        case 4:
+            return "⚃";
+        case 5:
+            return "⚄";
+        case 6:
+            return "⚅";
+        default:
+            "";
+            break;
+    }
+}
+
+rollButton.addEventListener(("click") , (e) => {
+    e.preventDefault();
+    roll = rollDice();
+    diceElement.innerText = roll;
+});
